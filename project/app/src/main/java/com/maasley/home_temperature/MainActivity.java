2d0f96f79c2c19aa712155bf9595abeed961df14
Steve <crossley_steven@hotmail.com>
2018-10-19 21:47:21 +0100

    Implemented data retrieval and updated the UI with data
    Added a means to obtain the temparature via. an ASyncTask to a subclass of the Temperature class, implemented methods in the Temperature class to retrieve the temperature and the last update time, added new text controls to the UI, and mapped those text controls to the methods in the temperature class to attach data to the UI.

--- project/app/src/main/java/com/maasley/home_temperature/MainActivity.java
+++ project/app/src/main/java/com/maasley/home_temperature/MainActivity.java
@@ -2,32 +2,26 @@ package com.maasley.home_temperature;
 
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
-import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends WearableActivity {
 
     private TextView mTextView;
+    private TextView dateText;
+    private Temperature t = new Temperature();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text);
-        mTextView.setText(getTempString());
+        mTextView = (TextView) findViewById(R.id.temp);
+        mTextView.setText(t.getTemp());
+
+        dateText = (TextView) findViewById(R.id.dateText);
+        dateText.setText(t.getLastUpdate());
 
         // Enables Always-on
         setAmbientEnabled();
-    }
-
-    public void onTap(View view) {
-        mTextView.setText(getTempString());
-    }
-
-    protected String getTempString() {
-        Temperature temperature = new Temperature();
-        temperature.getData();
-        return "0.00";
     }
 }
--- project/app/src/main/java/com/maasley/home_temperature/Temperature.java
+++ project/app/src/main/java/com/maasley/home_temperature/Temperature.java
@@ -17,25 +17,83 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.concurrent.ExecutionException;
 
 public class Temperature {
     String urlString = "http://192.168.1.5/api/EthzAaSJ2U85HX5iD3fiQaLhew9xGqyNh8h5RlFa/sensors/11";
 
-    public void getData() {
-        GetTemp gt = new GetTemp();
-        gt.execute();
+    private JSONObject getSensor() {
+
+        SensorRetriever retriever = new SensorRetriever();
+        JSONObject sensor = null;
+        try {
+            sensor = retriever.execute().get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return sensor;
     }
 
-    private class GetTemp extends AsyncTask<Void, Void, Double> {
+    public String getTemp() {
+        JSONObject sensor = getSensor();
+        JSONObject state = getSensorState(sensor);
+        String result = "";
+        try {
+            int tempInt = state.getInt("temperature");
+            Double temp = Math.round((tempInt*0.01) * 100d) /100d;
+            result = temp + "°C";
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public String getLastUpdate() {
+        JSONObject sensor = getSensor();
+        JSONObject state = getSensorState(sensor);
+        String result = "";
+        try {
+            String date = state.getString("lastupdated");
+            String year = date.substring(0,4);
+            String month = date.substring(5,7);
+            String day = date.substring(8,10);
+            String time = date.substring(11);
+            result = "Last updated " + day + "/" + month + "/" + year + " at " + time;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private JSONObject getSensorState(JSONObject sensor) {
+        JSONObject sensorState = null;
+        try {
+            sensorState = sensor.getJSONObject("state");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return sensorState;
+    }
+
+
+
+
+
+
+    private class SensorRetriever extends AsyncTask<Void, Void, JSONObject> {
         @Override
-        protected Double doInBackground(Void... voids) {
+        protected JSONObject doInBackground(Void... voids) {
+            JSONObject sensor = null;
+            String result = "error";
             try {
                 URL url = new URL(urlString);
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                 try {
                     InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-
-                    String random = "test";
+                    String jsonString = streamToString(in);
+                    sensor = getJsonFromString(jsonString);
                 } catch (Exception e) {
                     String s = e.toString();
                 } finally {
@@ -44,7 +102,33 @@ public class Temperature {
             } catch (Exception e) {
                 String s = e.toString();
             }
-            return 0.00;
+            return sensor;
+        }
+
+        private String streamToString(InputStream in) {
+            String result = "";
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            StringBuilder builder = new StringBuilder();
+            String line;
+            try {
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line).append('\n');
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            result = builder.toString();
+            return result;
+        }
+
+        private JSONObject getJsonFromString(String jsonString) {
+            JSONObject json = null;
+            try {
+                json = new JSONObject(jsonString);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return json;
         }
     }
 }
