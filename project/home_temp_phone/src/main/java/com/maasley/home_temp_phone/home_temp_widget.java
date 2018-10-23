b105f70243fb678829be48befacfad8cdb746599
Steve <crossley_steven@hotmail.com>
2018-10-23 17:30:09 +0100

    Created a basic widget
    Created a basic widget, though it requires a lot of work. Currently, it will display the temperature when first added to the home screen, although not much though has been put into the update process, and what happens after events like restarting the device.

--- project/home_temp_phone/src/main/java/com/maasley/home_temp_phone/home_temp_widget.java
+++ project/home_temp_phone/src/main/java/com/maasley/home_temp_phone/home_temp_widget.java
@@ -5,6 +5,8 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.widget.RemoteViews;
 
+import com.maasley.android_lib.Temperature;
+
 /**
  * Implementation of App Widget functionality.
  */
@@ -13,10 +15,11 @@ public class home_temp_widget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        Temperature t = new Temperature("http://192.168.1.5/api/EthzAaSJ2U85HX5iD3fiQaLhew9xGqyNh8h5RlFa/sensors/11");
+        CharSequence widgetText = t.getTemp();
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.home_temp_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setTextViewText(R.id.appwidget_temp, widgetText);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -32,6 +35,10 @@ public class home_temp_widget extends AppWidgetProvider {
 
     @Override
     public void onEnabled(Context context) {
+        Temperature t = new Temperature("http://192.168.1.5/api/EthzAaSJ2U85HX5iD3fiQaLhew9xGqyNh8h5RlFa/sensors/11");
+        CharSequence widgetText = t.getTemp();
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.home_temp_widget);
+        views.setTextViewText(R.id.appwidget_temp, widgetText);
         // Enter relevant functionality for when the first widget is created
     }
 
