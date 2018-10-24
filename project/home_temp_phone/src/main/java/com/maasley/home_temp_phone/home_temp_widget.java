47f22341b51c87d8fa33d33900ba839307656f34
Steve <crossley_steven@hotmail.com>
2018-10-24 16:54:41 +0100

    Made changes to how the temp is loaded for the widget.
    Made changes to how the temp is loaded for the widget.

--- project/home_temp_phone/src/main/java/com/maasley/home_temp_phone/home_temp_widget.java
+++ project/home_temp_phone/src/main/java/com/maasley/home_temp_phone/home_temp_widget.java
@@ -2,8 +2,10 @@ package com.maasley.home_temp_phone;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import com.maasley.android_lib.Temperature;
 
@@ -16,10 +18,10 @@ public class home_temp_widget extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
-
+        Toast toast = Toast.makeText(context,"Test",Toast.LENGTH_LONG);
+        toast.show();
         Temperature t = new Temperature("http://192.168.1.5/api/EthzAaSJ2U85HX5iD3fiQaLhew9xGqyNh8h5RlFa/sensors/11");
         CharSequence widgetText = t.getTemp();
-        widgetText = String.valueOf(getRandom());
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.home_temp_widget);
         views.setTextViewText(R.id.appwidget_temp, widgetText);
@@ -32,15 +34,15 @@ public class home_temp_widget extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-
             updateAppWidget(context, appWidgetManager, appWidgetId);
+
         }
     }
 
     @Override
     public void onEnabled(Context context) {
-        //updateTemp(context);
         // Enter relevant functionality for when the first widget is created
+
     }
 
     @Override
